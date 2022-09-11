85bbc370bdad582e5485da20a6e31bdff881c535
James Hall-Treworgy <jameshall-treworgy@Jamess-MacBook-Pro.local>
2022-09-11 10:31:16 -0400

    Added Exercise picker.

---
+++ src/app/workouts/components/exercise-picker/exercise-data/exercises.ts
@@ -0,0 +1,76 @@
+const Dumbell = 'https://static.thenounproject.com/png/1365947-200.png';
+
+export const exercises = [
+  {
+    title: 'Pull Ups',
+    image: Dumbell
+  },
+  {
+    title: 'Bench Press',
+    image: Dumbell
+  },
+  {
+    title: 'Inclinde Bench',
+    image: Dumbell
+  },
+  {
+    title: 'Barbel Squat',
+    image: Dumbell
+  },
+  {
+    title: 'Incline Press',
+    image: Dumbell
+  },
+  {
+    title: 'Bulgarian Squat',
+    image: Dumbell
+  },
+  {
+    title: 'Deadlift',
+    image: Dumbell
+  },
+  {
+    title: 'Romanian Deadlift',
+    image: Dumbell
+  },
+  {
+    title: 'Straight Leg Deadlift',
+    image: Dumbell
+  },
+  {
+    title: 'Barbel Curl',
+    image: Dumbell
+  },
+  {
+    title: 'Hip Thrusts',
+    image: Dumbell
+  },
+  {
+    title: 'Lat Pulldown',
+    image: Dumbell
+  },
+  {
+    title: 'Bent Over Rows',
+    image: Dumbell
+  },
+  {
+    title: 'Chest Fly',
+    image: Dumbell
+  },
+  {
+    title: 'Tricep Extension',
+    image: Dumbell
+  },
+  {
+    title: 'Bicep Curl',
+    image: Dumbell
+  },
+  {
+    title: 'Lateral Raise',
+    image: Dumbell
+  },
+  {
+    title: 'Dips',
+    image: Dumbell
+  },
+]
