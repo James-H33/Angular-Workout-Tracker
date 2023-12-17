2005e377415257da806328f111c419233c0dae7f
James Hall-Treworgy <jameshall-treworgy@Jamess-MBP.lan>
2023-12-17 13:21:04 -0500

    Removed unused code.

--- src/app/workouts/store/workout/workout.selectors.ts
+++ src/app/workouts/store/workout/workout.selectors.ts
@@ -1,12 +1,5 @@
 import { WorkoutDetailFeature } from "./workout.reducer";
 
-// export const selectWorkoutState = createFeatureSelector<IWorkoutDetailState>(WorkoutStateKeys.WorkoutDetail);
-
-// export const selectCurrentWorkout = createSelector(
-//   selectWorkoutState,
-//   (state) => state.workout
-// )
-
 export const {
   selectWorkout,
   selectIsLoading
